d8ecffd245524cddfca77ae80e35f961e694eeb7
JAIRAMEZCUA <enriquejairamezcuamaciasg3@outlook.com>
2023-09-01 00:55:37 -0600

    init commit

---
+++ app/src/main/java/com/example/navigationcomponent/SecondFragment.kt
@@ -0,0 +1,36 @@
+package com.example.navigationcomponent
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [SecondFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class SecondFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val root = inflater.inflate(R.layout.fragment_second, container, false)
+        root.findViewById<Button>(R.id.button).setOnClickListener {
+            findNavController().navigate(R.id.action_secondFragment_to_thirdFragment)
+        }
+        return root
+    }
+
+}
